7d8dc50f2091e1ff925427859a51f6dc7f16f994
isaiga0101 <gayfieldfamily@hotmail.com>
2016-01-02 12:50:27 -0800

    Make led turn on when low light instead of high light.

--- main.asm
+++ main.asm
@@ -22,9 +22,9 @@ AD:
 	ldi r16, (1 << led)	; Load r16 with bit for led
 	out ddrb, r16		; Make led output and other pins input
 	nop					; Wait for ddrb to change
-	sbi portb, led		; Turn on led
+	cbi portb, led		; Turn on led
 	rcall delay_1ms		; Delay_1ms 
-	cbi portb, led		; Clear led
+	sbi portb, led		; Clear led
     rjmp AD				; relative jump to AD, loop until interupt
 
 
